f05c511ae73553db2fc11fd34d9638f3d81beb03
shulmanbrent <shulmanbrent@yahoo.com>
2014-12-13 16:25:29 -0500

    created feature developer

--- feature_developer.py
+++ feature_developer.py
@@ -3,6 +3,8 @@ import csv
 import json
 import gzip
 import parse
+import string
+
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -20,6 +22,22 @@ with open('testing_data_batch_results.csv', 'r') as results:
         dic = dict(zip(headers, review))
         response_data.append(dic)
 
-for item in response_data:
-    print item['Answer'] 
+def featureDeveloper(response_data):
+    exclude = set(string.punctuation)
+    featureSets = []    
     
+    for item in response_data:
+        words = item['Review'].split(' ')
+        for w in words:
+            if (not w.isalpha()):
+                w = ''.join(ch for ch in w if ch not in exclude)
+            featureSets.append((w, item['Answer']))
+    
+    return featureSets
+    
+featureSets = featureDeveloper(response_data)
+
+print featureSets
+
+
+
